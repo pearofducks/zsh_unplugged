6b879eacaaa46392eb837f6680b2828cc8091a7c
Dave Honneffer <pearofducks@gmail.com>
2022-01-12 19:40:24 +0100

    Update zsh_unplugged.plugin.zsh

--- zsh_unplugged.plugin.zsh
+++ zsh_unplugged.plugin.zsh
@@ -24,6 +24,26 @@ function plugin-load() {
   done
 }
 
+# only download, don't source
+function plugin-download() {
+  local repo plugin_name plugin_dir initfile initfiles
+  ZPLUGINDIR=${ZPLUGINDIR:-${ZDOTDIR:-$HOME/.config/zsh}/plugins}
+  for repo in $@; do
+    plugin_name=${repo:t}
+    plugin_dir=$ZPLUGINDIR/$plugin_name
+    initfile=$plugin_dir/$plugin_name.plugin.zsh
+    if [[ ! -d $plugin_dir ]]; then
+      echo "Cloning $repo"
+      git clone -q --depth 1 --recursive --shallow-submodules https://github.com/$repo $plugin_dir
+    fi
+    if [[ ! -e $initfile ]]; then
+      initfiles=($plugin_dir/*.plugin.{z,}sh(N) $plugin_dir/*.{z,}sh{-theme,}(N))
+      [[ ${#initfiles[@]} -gt 0 ]] || { echo >&2 "Plugin has no init file '$repo'." && continue }
+      ln -s "${initfiles[1]}" "$initfile"
+    fi
+  done
+}
+
 # if you want to compile your plugins you may see performance gains
 function plugin-compile() {
   ZPLUGINDIR=${ZPLUGINDIR:-${ZDOTDIR:-$HOME/.config/zsh}/plugins}
